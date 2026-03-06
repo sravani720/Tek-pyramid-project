5f4150333580a82b37e20c8b593ecf2398ba672d
Vinay Lanka <Vinay Lanka@Vinay-Lankaa>
2026-03-06 12:27:50 +0530

    new class 9

---
+++ mavenpracticeproject/src/test/java/mavenpracticeproject/Sample9.java
@@ -0,0 +1,11 @@
+package mavenpracticeproject;
+
+public class Sample9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("------welcome-----");
+
+	}
+
+}
