577ea22523297fdd4f1da2e9d73bf5572a2e982a
KARTHIK M <KARTHIK M@LAPTOP-FG6TU5Q7>
2026-03-06 13:29:14 +0530

    commit

--- mavenpracticeproject/src/test/java/mavenpracticeproject/Sample9.java
+++ mavenpracticeproject/src/test/java/mavenpracticeproject/Sample9.java
@@ -5,7 +5,7 @@ public class Sample9 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("------welcome-----");
-		System.out.println("welcome to tekpyramid");
+		System.out.println("welcome to tek");
 
 	}
 
